ab29b934bdd9a205f200f459d1faa8e77c205678
Nick Karadzha <nick.karadzha@jenza.com>
2025-06-03 21:59:11 +0100

    use theme for constants;

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './components/App';
-import GlobalStyles from './components/GlobalStyles';
+import App from "./components/App";
+import GlobalStyles from "./components/GlobalStyles"
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./styles/theme";
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     <GlobalStyles />
   </StrictMode>
 );
